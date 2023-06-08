1e6d1338d5bc06ace72778381cf47be878a3c23c
Insane Dev <89750356+PlasmicRays@users.noreply.github.com>
2023-06-08 11:09:37 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x = document.getElementById("myAudio");
+
+
+function new_image() {
+    block_image_object.set({
+        top: 0,
+        left: 0
+        canvas.add(block_image_object),
+    });
+
+
+
+}
+
+function playSound() {
+
+    x.play("myAudio");
+
+}
